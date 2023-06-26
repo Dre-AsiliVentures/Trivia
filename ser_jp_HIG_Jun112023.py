406c5a2c825814f5513ed5cdba82dc9a1ef80112
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-26 08:39:30 +0300

    Update ser_jp_HIG_Jun112023.py
    Use iloc

--- ser_jp_HIG_Jun112023.py
+++ ser_jp_HIG_Jun112023.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 st.write(" Trivia: Sermon by Joseph Prince: There is Hope in the Grace of God")
 # Load the Excel file
-#df = pd.read_excel('folder/text.xlsx')
 df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/06/JosephPrince_There_is_Hope_in_God.xlsx')
 
 # Get unique chapters
@@ -15,38 +14,34 @@ chapters = df['Chapter'].unique()
 # Initialize score
 score = 0
 
-# Iterate over unique chapters
-for chapter in chapters:
-    # Create a container for the current chapter
-    chapter_container = st.container()
-    with chapter_container:
-        st.header(chapter)
-        
-        # Filter questions for the current chapter
-        chapter_questions = df[df['Chapter'] == chapter]
-        
-        # Iterate over the questions for the current chapter
-        for index, question in chapter_questions.iterrows():
-            st.subheader(f"Q{index+1}: {question['Question']}")
-            
-            # Display the options as radio buttons
-            choice = st.radio("Choose an option:",
-                              (question['Option A'], question['Option B'], question['Option C'], question['Option D']))
-            
-            # Check if the selected option matches the correct answer
-            if choice == question['Correct Answer']:
-                st.write("Correct!")
-                score += 1  # Increment the score
-            
-            # Add an option to choose the word hint
-            if st.button("Word Hint"):
-                st.write(f"Hint: {question['Word Hint']}")
-            
-            # Add an option to choose the time hint
-            if st.button("Time Hint"):
-                st.write(f"Hint: {question['Time Hint']}")
-            
-            st.write("---")
+# Create a dictionary to store the questions and answers for each chapter
+chapter_questions = {}
+for i in range(len(df)):
+  chapter = df.loc[i, "Chapter"]
+  question = df.loc[i, "Question"]
+  answer = df.loc[i, "Correct Answer"]
+  options = [df.loc[i, "Option A"], df.loc[i, "Option B"], df.loc[i, "Option C"], df.loc[i, "Option D"]]
+  chapter_questions[chapter] = {"question": question, "answer": answer, "options": options}
+
+# Create a container for each chapter
+for chapter in chapter_questions:
+  st.container(title=chapter)
+  st.write(chapter_questions[chapter]["question"])
+
+  # Create radio buttons for the options
+  radio_buttons = st.radio("Select an option:", chapter_questions[chapter]["options"])
+
+  # If the option selected matches the correct answer, update the score
+  if radio_buttons == chapter_questions[chapter]["answer"]:
+    st.write("Correct!")
+  else:
+    st.write("Incorrect. The correct answer is " + chapter_questions[chapter]["answer"])
+
+  # Show the word hint or the time hint
+  if st.button("Word hint"):
+    st.write(chapter_questions[chapter]["Word Hint"])
+  elif st.button("Time hint"):
+    st.write(chapter_questions[chapter]["Time Hint"])
 
 # Display the final score
 st.write(f"Final Score: {score}/{len(df)}")
