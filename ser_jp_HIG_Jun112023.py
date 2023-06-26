8697517c606829a7d4d3d3edb371ac73e5b61ff7
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-26 09:39:52 +0300

    Update ser_jp_HIG_Jun112023.py

--- ser_jp_HIG_Jun112023.py
+++ ser_jp_HIG_Jun112023.py
@@ -15,33 +15,33 @@ df.fillna("", inplace=True)
 
 # Initialize the score
 total_score = 0
-
 # Display each question
 for index, row in df.iterrows():
-    st.write(f"**Question {index+1}:** {row['Question']}")
+    question = row['Question']
+    if pd.isnull(question) or question.strip() == "":
+        chapter = row['Chapter']
+        st.write(f"**Chapter {chapter}:**")
+    else:
+        st.write(f"**Question {index+1}:** {question}")
+        # Display options
+        option = st.radio("Select an option:", options=[row['Option A'], row['Option B'], row['Option C'], row['Option D']])
 
-    # Display options
-    option = st.radio(f"Select an option for Question {index+1}:", 
-                      options=[row['Option A'], row['Option B'], row['Option C'], row['Option D']],
-                      key=f"option_{index}")
+        # Display hints
+        if st.button("Word Hint", key=f"word_hint_{index}"):
+            st.write(f"Word Hint: {row['Word']}")
+        if st.button("Time Hint", key=f"time_hint_{index}"):
+            st.write(f"Time Hint: {row['Time Hint']}")
 
-    # Display hints
-    if st.button("Word Hint", key=f"word_hint_{index}"):
-        st.write(f"Word Hint: {row['Word']}")
-    if st.button("Time Hint", key=f"time_hint_{index}"):
-        st.write(f"Time Hint: {row['Time Hint']}")
-
-    # Check answer
-    if st.button("Check Answer", key=f"check_answer_{index}"):
-        if option == row['Correct Answer']:
-            st.write("Correct!")
-            total_score += 1
-        else:
-            st.write("Incorrect.")
+        # Check answer
+        if st.button("Check Answer", key=f"check_answer_{index}"):
+            if option == row['Correct Answer']:
+                st.write("Correct!")
+                total_score += 1
+            else:
+                st.write("Incorrect.")
 
     st.write("---")
 
 # Display total quiz score
 st.write(f"Total Score: {total_score}/{len(df)}")
-
 
