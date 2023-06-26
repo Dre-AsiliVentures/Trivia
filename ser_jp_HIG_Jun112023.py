76acdd2c864a5eb4e132d53e04e8379323bd3e12
Dre Asili <61284769+Dre-AsiliVentures@users.noreply.github.com>
2023-06-26 06:29:59 +0300

    Update ser_jp_HIG_Jun112023.py

--- ser_jp_HIG_Jun112023.py
+++ ser_jp_HIG_Jun112023.py
@@ -12,41 +12,41 @@ df = pd.read_excel('https://asiliventures.com/wp-content/uploads/2023/06/JosephP
 # Get unique chapters
 chapters = df['Chapter'].unique()
 
-# Create a container for each unique chapter
+# Initialize score
+score = 0
+
+# Iterate over unique chapters
 for chapter in chapters:
-    chapter_questions = df[df['Chapter'] == chapter]
-    
-    # Display the chapter name as a header
-    st.header(chapter)
-    
-    # List all the questions for the chapter
-    for index, question in chapter_questions.iterrows():
-        st.subheader(f"Q{index+1}: {question['Question']}")
+    # Create a container for the current chapter
+    chapter_container = st.container()
+    with chapter_container:
+        st.header(chapter)
         
-        # Display the options as radio buttons
-        choice = st.radio("Choose an option:",
-                          (question['Option A'], question['Option B'], question['Option C'], question['Option D']))
+        # Filter questions for the current chapter
+        chapter_questions = df[df['Chapter'] == chapter]
         
-        # Check if the selected option matches the correct answer
-        if choice == question['Correct Answer']:
-            st.write("Correct!")
-            # Update the score accordingly
-            # Update the score accordingly
-            score = st.session_state.get('score', 0)
-            st.session_state['score'] = score + 1
-        else:
-            st.write("Incorrect!")
-        
-        # Add an option to choose the word hint
-        if st.button("Word Hint"):
-            st.write(f"Hint: {question['Word Hint']}")
-        
-        # Add an option to choose the time hint
-        if st.button("Time Hint"):
-            st.write(f"Hint: {question['Time Hint']}")
-        
-        st.write("---")
+        # Iterate over the questions for the current chapter
+        for index, question in chapter_questions.iterrows():
+            st.subheader(f"Q{index+1}: {question['Question']}")
+            
+            # Display the options as radio buttons
+            choice = st.radio("Choose an option:",
+                              (question['Option A'], question['Option B'], question['Option C'], question['Option D']))
+            
+            # Check if the selected option matches the correct answer
+            if choice == question['Correct Answer']:
+                st.write("Correct!")
+                score += 1  # Increment the score
+            
+            # Add an option to choose the word hint
+            if st.button("Word Hint"):
+                st.write(f"Hint: {question['Word Hint']}")
+            
+            # Add an option to choose the time hint
+            if st.button("Time Hint"):
+                st.write(f"Hint: {question['Time Hint']}")
+            
+            st.write("---")
 
 # Display the final score
-score = st.session_state.get('score', 0)
 st.write(f"Final Score: {score}/{len(df)}")
